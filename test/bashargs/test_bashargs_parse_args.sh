cc891c98039ef49835fea9ee1604937e45237131
Jeff Fredenburg <jfreden@fnal.gov>
2023-10-29 19:30:42 -0500

    added tests for repeated parse arg calls

--- test/bashargs/test_bashargs_parse_args.sh
+++ test/bashargs/test_bashargs_parse_args.sh
@@ -5,3 +5,17 @@ function test_bashargs_parse_args_no_args() {
     $(bashargs::parse_args) || return_code=1
     [[ $return_code -eq 0 ]]
 }
+
+function test_bashargs_parse_args_repeated_parse_with_then_without_flag() {
+    bashargs::add_optional_flag --argname
+    bashargs::parse_args --argname
+    bashargs::parse_args
+    [[ $(bashargs::get_arg --argname) == "false" ]]
+}
+
+function test_bashargs_parse_args_repeated_parse_without_then_with_flag() {
+    bashargs::add_optional_flag --argname
+    bashargs::parse_args
+    bashargs::parse_args --argname
+    [[ $(bashargs::get_arg --argname) == "true" ]]
+}
