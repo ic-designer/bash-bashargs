280128dcac344b3b0244f8d066c00d8b61481448
Jeff Fredenburg <jfreden@fnal.gov>
2023-12-29 16:52:17 -0600

    refactored the makefile to create standard targets and a standard organization

---
+++ make/private.mk
@@ -0,0 +1,52 @@
+# Required Paths
+DIR_BUILD ?= $(error ERROR: Undefined variable DIR_BUILD)
+DIR_BUILD_PKGS ?= $(error ERROR: Undefined variable DIR_BUILD_PKGS)
+DIR_BUILD_TEST ?= $(error ERROR: Undefined variable DIR_BUILD_TEST)
+
+
+# Includes
+include make/extras.mk
+
+
+# Dependencies
+WAXWING:=$(DIR_BUILD_PKGS)/waxwing/bin/waxwing
+$(WAXWING): |$(DIR_BUILD_PKGS)/.
+	$(call git-clone-shallow, \
+			git@github.com:jfredenburg/waxwing.git, \
+			$(DIR_BUILD_PKGS)/waxwing, \
+			main)
+
+
+# Private targets
+.PHONY: private_test
+private_test: \
+		$(DIR_BUILD_TEST)/test-bashargs.merged.sh \
+		$(WAXWING) \
+		|$(DIR_BUILD_TEST)/.
+	$(WAXWING) $(DIR_BUILD_TEST)
+
+$(DIR_BUILD_TEST)/test-bashargs.merged.sh: \
+		src/bashargs/bashargs.sh \
+		$(shell find test/bashargs -name 'test_bashargs*.sh') \
+		| $(DIR_BUILD_TEST)/.
+	$(build-merged-script)
+
+
+.PHONY: private_clean
+private_clean:
+	@echo "Cleaning directories: $(DIR_BUILD), $(DIR_BUILD_PKGS), $(DIR_BUILD_TEST)"
+	$(if $(wildcard $(DIR_BUILD)), rm -rf $(DIR_BUILD))
+	$(if $(wildcard $(DIR_BUILD_PKGS)), rm -rf $(DIR_BUILD_PKGS))
+	$(if $(wildcard $(DIR_BUILD_TEST)), rm -rf $(DIR_BUILD_TEST))
+
+
+.PHONY: private_pkg_list
+private_pkg_list:
+	$(call git-list-remotes, $(DIR_BUILD_PKGS))
+
+
+.PHONY: private_pkg_override
+private_pkg_override: REPO ?= $(error ERROR: Repo not defined. Please defined REPO=<repository>)
+private_pkg_override: NAME ?= $(error ERROR: Name not defined. Please defined NAME=<repository>)
+private_pkg_override:
+	$(call git-clone-shallow, $(REPO), $(DIR_BUILD_PKGS)/$(NAME))
--- makefile
+++ makefile
@@ -1,33 +1,34 @@
+override VERSION:=0.1.0+20231228
 
+.PHONY: check
+check: private_test
+
+.PHONY: clean
+clean: private_clean
+
+.PHONY: pkg_list
+pkg_list: private_pkg_list
+
+.PHONY: pkg_override
+pkg_override: private_pkg_override
+
+.PHONY: test
+test: private_test
+
+
+# Config
+.DELETE_ON_ERROR:
+.SUFFIXES:
+MAKEFLAGS += --no-builtin-rules
+SHELL = /usr/bin/env bash
+.SHELLFLAGS += -e -o pipefail
+
+
+# Paths
 DIR_BUILD:=.make
-DIR_SRC:=src
-DIR_TEST:=test
-
-DIR_BUILD_DEPS:=$(DIR_BUILD)/deps
+DIR_BUILD_PKGS:=$(DIR_BUILD)/pkgs
 DIR_BUILD_TEST:=$(DIR_BUILD)/test
 
-LIBS_SRC:=$(shell find $(DIR_SRC) -name '*.sh')
-LIBS_TEST:=$(shell find $(DIR_TEST) -name 'test*.sh')
-OBJ_TEST_ALL:=$(DIR_BUILD_TEST)/test_bashargs_all.sh
 
-
-WAXWING_PATH:=$(DIR_BUILD_DEPS)/waxwing
-WAXWING_BIN:=$(WAXWING_PATH)/bin/waxwing
-WAXWING_URL:=git@github.com:ic-designer/waxwing.git
-$(WAXWING_BIN): | $(WAXWING_PATH)/.
-	git clone  $(WAXWING_URL) $(WAXWING_PATH)
-
-
-.DELETE_ON_ERROR:
-.PHONY: test
-test: $(OBJ_TEST_ALL) $(WAXWING_BIN)
-	$(WAXWING_BIN) $(dir $(OBJ_TEST_ALL))
-
-$(OBJ_TEST_ALL): $(LIBS_SRC) $(LIBS_TEST) | $(dir $(OBJ_TEST_ALL))/.
-	cat $^ > $@
-
-%/. :
-	mkdir -p $@
-
-clean:
-	rm -rf $(DIR_BUILD)
+# Includes
+include make/private.mk
