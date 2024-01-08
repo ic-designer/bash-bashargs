aed428ced00ecf292359026136beb33f382089ab
Jeff Fredenburg <jfreden@fnal.gov>
2024-01-07 22:43:20 -0600

    Created targets and directory variables for installing the bashargs library

--- Makefile
+++ Makefile
@@ -4,19 +4,32 @@
 MAKEFLAGS += --no-builtin-rules
 
 # Constants
+DESTDIR =
+PREFIX = $(HOME)/.local
+LIBDIR = $(PREFIX)/lib
+
 override NAME := bashargs
 override VERSION := $(shell git describe --always --dirty --broken)
 override WORKDIR_ROOT := $(CURDIR)/.make
 
 #Targets
+.PHONY: all
+all: private_all
+
 .PHONY: check
 check: private_test
 
 .PHONY: clean
 clean: private_clean
 
+.PHONY: install
+install: private_install
+
 .PHONY: test
 test: private_test
+
+.PHONY: uninstall
+uninstall: private_uninstall
 
 #Includes
 include make/private.mk
--- make/private.mk
+++ make/private.mk
@@ -1,53 +1,82 @@
 # Constants
+DESTDIR ?= $(error ERROR: Undefined variable DESTDIR)
+PREFIX ?= $(error ERROR: Undefined variable PREFIX)
+LIBDIR ?= $(error ERROR: Undefined variable LIBDIR)
 NAME ?= $(error ERROR: Undefined variable NAME)
 VERSION ?= $(error ERROR: Undefined variable VERSION)
 WORKDIR_ROOT ?= $(error ERROR: Undefined variable WORKDIR_ROOT)
-override WORKDIR = $(WORKDIR_ROOT)/$(NAME)-$(VERSION)
-override WORKDIR_PKGS = $(WORKDIR)/pkgs
+override PKGSUBDIR = $(NAME)/$(NAME)-$(VERSION)
+override WORKDIR = $(WORKDIR_ROOT)
+override WORKDIR_BUILD = $(WORKDIR)/build
+override WORKDIR_DEPS = $(WORKDIR)/deps
 override WORKDIR_TEST = $(WORKDIR)/test
 
 # Includes
 include make/extras.mk
 
 # Dependencies
-WAXWING:=$(WORKDIR_PKGS)/waxwing/bin/waxwing
-$(WAXWING): |$(WORKDIR_PKGS)/.
+WAXWING:=$(WORKDIR_DEPS)/waxwing/bin/waxwing
+$(WAXWING): |$(WORKDIR_DEPS)/.
 	$(call git-clone-shallow, \
 			git@github.com:ic-designer/waxwing.git, \
-			$(WORKDIR_PKGS)/waxwing, \
+			$(WORKDIR_DEPS)/waxwing, \
 			main)
 
 # Targets
+.PHONE: private_all
+private_all: $(WORKDIR_BUILD)/bashargs.sh
+
+$(WORKDIR_BUILD)/bashargs.sh: src/bashargs/bashargs.sh
+	$(call install-as-copy)
+
+
 .PHONY: private_clean
 private_clean:
-	@echo "Cleaning directories: $(WORKDIR_ROOT), $(WORKDIR), $(WORKDIR_PKGS), $(WORKDIR_TEST)"
-	$(if $(wildcard $(WORKDIR)), rm -rf $(WORKDIR))
-	$(if $(wildcard $(WORKDIR_PKGS)), rm -rf $(WORKDIR_PKGS))
-	$(if $(wildcard $(WORKDIR_ROOT)), rm -rf $(WORKDIR_ROOT))
-	$(if $(wildcard $(WORKDIR_TEST)), rm -rf $(WORKDIR_TEST))
+	@echo "Cleaning directories:"
+	@$(if $(wildcard $(WORKDIR)), rm -rfv $(WORKDIR))
+	@$(if $(wildcard $(WORKDIR_DEPS)), rm -rfv $(WORKDIR_DEPS))
+	@$(if $(wildcard $(WORKDIR_ROOT)), rm -rfv $(WORKDIR_ROOT))
+	@$(if $(wildcard $(WORKDIR_TEST)), rm -rfv $(WORKDIR_TEST))
+	@echo
+
+
+.PHONY: private_install
+private_install: $(DESTDIR)/$(LIBDIR)/$(PKGSUBDIR)/bashargs.sh
+
+$(DESTDIR)/$(LIBDIR)/$(PKGSUBDIR)/bashargs.sh: $(WORKDIR_BUILD)/bashargs.sh
+	$(call install-as-copy)
+
+
+.PHONY: private_test
+private_test: $(WAXWING) $(WORKDIR_TEST)/test-bashargs.sh
+	$(WAXWING) $(WORKDIR_TEST)
+
+$(WORKDIR_TEST)/test-bashargs.sh: \
+		$(WORKDIR_TEST)/$(LIBDIR)/$(PKGSUBDIR)/bashargs.sh \
+		$(shell find test/bashargs -name 'test_bashargs*.sh')
+	$(build-bash-library)
+
+ifneq ($(DESTDIR),  $(WORKDIR_TEST))
+$(WORKDIR_TEST)/$(LIBDIR)/$(PKGSUBDIR)/bashargs.sh:
+	@$(MAKE) install DESTDIR=$(abspath $(WORKDIR_TEST))
+endif
+
+
+.PHONY: private_uninstall
+private_uninstall:
+	@echo "Uninstalling $(NAME)"
+	@\rm -rdfv $(DESTDIR)/$(LIBDIR)/$(PKGSUBDIR) 2> /dev/null || true
+	@\rm -dv $(dir $(DESTDIR)/$(LIBDIR)/$(PKGSUBDIR)) 2> /dev/null || true
+	@\rm -dv $(DESTDIR)/$(LIBDIR) 2> /dev/null || true
 
 
 .PHONY: private_pkg_list
 private_pkg_list:
-	$(call git-list-remotes, $(WORKDIR_PKGS))
+	$(call git-list-remotes, $(WORKDIR_DEPS))
 
 
 .PHONY: private_pkg_override
 private_pkg_override: REPO_NAME ?= $(error ERROR: Name not defined. Please defined REPO_NAME=<name>)
 private_pkg_override: REPO_PATH ?= $(error ERROR: Repo not defined. Please defined REPO_PATH=<path>)
 private_pkg_override:
-	$(call git-clone-shallow, $(REPO_PATH), $(WORKDIR_PKGS)/$(REPO_NAME))
-
-
-.PHONY: private_test
-private_test: \
-		$(WORKDIR_TEST)/test-bashargs.sh \
-		$(WAXWING) \
-		|$(WORKDIR_TEST)/.
-	$(WAXWING) $(WORKDIR_TEST)
-
-$(WORKDIR_TEST)/test-bashargs.sh: \
-		src/bashargs/bashargs.sh \
-		$(shell find test/bashargs -name 'test_bashargs*.sh') \
-		| $(WORKDIR_TEST)/.
-	$(build-bash-library)
+	$(call git-clone-shallow, $(REPO_PATH), $(WORKDIR_DEPS)/$(REPO_NAME))
