bd2983697b51c5b9bca732c5b4686d97ad354737
Jeff Fredenburg <jfreden@fnal.gov>
2024-01-07 22:36:38 -0600

    common recipes in make/extras.mk now more verbose

--- make/extras.mk
+++ make/extras.mk
@@ -1,17 +1,28 @@
 define build-bash-library
 	@echo 'Bulding library $@'
+	@mkdir -p $(dir $@)
 	@cat $^ > $@
+	@echo
 endef
 
 define git-clone-shallow
 	$(if $(wildcard $2), rm -rf $2)
-	@git clone -qv --depth=1 $1 $2 $(if $3, --branch $3)
+	git clone -v --depth=1 $1 $2 $(if $3, --branch $3)
+	@echo
 endef
 
 define git-list-remotes
 	@for d in $(1)/*; do \
 		echo "$${d} $$(git -C $${d} remote get-url origin --push)"; \
 	done
+endef
+
+define install-as-copy
+	@install -dv $(dir $@)
+	@install -Sv $< $@
+	test -f $@
+	diff $@ $<
+	@echo
 endef
 
 
