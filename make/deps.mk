cd8c615a8f2e704ad7f4de079812c214c4f64ca5
Jeff Fredenburg <jfreden@fnal.gov>
2024-01-10 17:35:08 -0600

    Updated waxwing and boxerbird to just take the main branch since they are still in development

--- make/deps.mk
+++ make/deps.mk
@@ -5,13 +5,11 @@ WORKDIR_DEPS ?= $(error ERROR: Undefined variable WORKDIR_DEPS)
 override WAXWING := $(WORKDIR_DEPS)/bash-waxwing/bin/waxwing
 $(WAXWING):
 	@echo "Loading Waxwing..."
-	git clone --config advice.detachedHead=false \
-		git@github.com:ic-designer/bash-waxwing.git --branch main $(WORKDIR_DEPS)/bash-waxwing
+	git clone git@github.com:ic-designer/bash-waxwing.git $(WORKDIR_DEPS)/bash-waxwing
 	@echo
 
 override BOXERBIRD.MK := $(WORKDIR_DEPS)/make-boxerbird/boxerbird.mk
 $(BOXERBIRD.MK):
 	@echo "Loading Boxerbird..."
-	git clone --config advice.detachedHead=false \
-		git@github.com:ic-designer/make-boxerbird.git --branch 0.1.0 $(WORKDIR_DEPS)/make-boxerbird
+	git clone git@github.com:ic-designer/make-boxerbird.git $(WORKDIR_DEPS)/make-boxerbird
 	@echo
