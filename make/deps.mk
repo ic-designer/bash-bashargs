0bf6ac735d902feac49071de660a61567ab4d67b
Jeff Fredenburg <jfreden@fnal.gov>
2024-01-10 21:25:20 -0600

    Created Makefile variables for BOXERBIRD_BRANCH and WAXWING_BRANCH

--- make/deps.mk
+++ make/deps.mk
@@ -1,15 +1,18 @@
 # Constants
 WORKDIR_DEPS ?= $(error ERROR: Undefined variable WORKDIR_DEPS)
 
-# Dependencies
-override WAXWING := $(WORKDIR_DEPS)/bash-waxwing/bin/waxwing
-$(WAXWING):
-	@echo "Loading Waxwing..."
-	git clone git@github.com:ic-designer/bash-waxwing.git $(WORKDIR_DEPS)/bash-waxwing
-	@echo
+BOXERBIRD_BRANCH := main
+WAXWING_BRANCH := main
 
+# Dependencies
 override BOXERBIRD.MK := $(WORKDIR_DEPS)/make-boxerbird/boxerbird.mk
 $(BOXERBIRD.MK):
 	@echo "Loading Boxerbird..."
-	git clone git@github.com:ic-designer/make-boxerbird.git $(WORKDIR_DEPS)/make-boxerbird
+	git clone git@github.com:ic-designer/make-boxerbird.git --branch $(BOXERBIRD_BRANCH) $(WORKDIR_DEPS)/make-boxerbird
+	@echo
+
+override WAXWING := $(WORKDIR_DEPS)/bash-waxwing/bin/waxwing
+$(WAXWING):
+	@echo "Loading Waxwing..."
+	git clone git@github.com:ic-designer/bash-waxwing.git --branch $(WAXWING_BRANCH) $(WORKDIR_DEPS)/bash-waxwing
 	@echo
