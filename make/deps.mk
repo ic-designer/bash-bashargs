0d6a35bb13a721de7bd6ce4538fda56046498d11
Jeff Fredenburg <jfreden@fnal.gov>
2024-01-10 23:22:06 -0600

    converted boxerbird and waxwing to use public https URLs

--- make/deps.mk
+++ make/deps.mk
@@ -8,11 +8,15 @@ WAXWING_BRANCH := main
 override BOXERBIRD.MK := $(WORKDIR_DEPS)/make-boxerbird/boxerbird.mk
 $(BOXERBIRD.MK):
 	@echo "Loading Boxerbird..."
-	git clone git@github.com:ic-designer/make-boxerbird.git --branch $(BOXERBIRD_BRANCH) $(WORKDIR_DEPS)/make-boxerbird
+	git clone --config advice.detachedHead=false --depth 1 \
+			https://github.com/ic-designer/make-boxerbird.git --branch $(BOXERBIRD_BRANCH) \
+			$(WORKDIR_DEPS)/make-boxerbird
 	@echo
 
 override WAXWING := $(WORKDIR_DEPS)/bash-waxwing/bin/waxwing
 $(WAXWING):
 	@echo "Loading Waxwing..."
-	git clone git@github.com:ic-designer/bash-waxwing.git --branch $(WAXWING_BRANCH) $(WORKDIR_DEPS)/bash-waxwing
+	git clone  --config advice.detachedHead=false --depth 1 \
+			https://github.com/ic-designer/bash-waxwing.git --branch $(WAXWING_BRANCH) \
+			$(WORKDIR_DEPS)/bash-waxwing
 	@echo
