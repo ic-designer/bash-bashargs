532b6c6cb87ac8ad9d23fb8da82238d584dce53f
Jeff Fredenburg <jfreden@fnal.gov>
2024-06-11 12:55:50 -0500

    the test script used by waxwing is now built in a target specific path

--- test/bashargs/test_bashargs_waxwing.mk
+++ test/bashargs/test_bashargs_waxwing.mk
@@ -4,11 +4,12 @@ WORKDIR_BUILD ?= $(error ERROR: Undefined variable WORKDIR_TEST)
 WORKDIR_TEST ?= $(error ERROR: Undefined variable WORKDIR_TEST)
 
 # Targets
-test-bashargs-waxwing: $(WAXWING) $(WORKDIR_TEST)/test-bashargs.sh
+.SECONDEXPANSION:
+test-bashargs-waxwing: $$(WAXWING) $$(WORKDIR_TEST)/$$@/test-bashargs.sh
 	$(WAXWING) $(WORKDIR_TEST)
 
-.NOTPARALLEL: $(WORKDIR_TEST)/test-bashargs.sh
-$(WORKDIR_TEST)/test-bashargs.sh: \
+.NOTPARALLEL: $(WORKDIR_TEST)/%/test-bashargs.sh
+$(WORKDIR_TEST)/%/test-bashargs.sh: \
 		$(WORKDIR_BUILD)/bashargs.sh \
 		$(shell find test/bashargs -name 'test_bashargs*.sh')
 	$(call bowerbird::build-bash-library)
